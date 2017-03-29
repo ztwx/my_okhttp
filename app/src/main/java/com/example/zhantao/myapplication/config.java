f277ba84dc1d7c2057f825a20ef6047f2513ddcc
ztwx <ztyrwx@gmail.com>
2017-03-29 16:35:21 +0800

    picasso

---
+++ app/src/main/java/com/example/zhantao/myapplication/config.java
@@ -0,0 +1,10 @@
+package com.example.zhantao.myapplication;
+
+/**
+ * Created by zhantao on 2017/3/29.
+ */
+public class config {
+    public static class API{
+        public static final String BESE_URL="http//:192.168.0.104:5000/";
+    }
+}
